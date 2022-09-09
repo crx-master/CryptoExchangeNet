243d10cea9ecf863725d8d48e5b0284ace3999a1
Luciano Carvalho <luciano@crucial.com.br>
2022-09-09 09:48:35 -0300

    Rename files and namespacesd

---
+++ CryptoExchangeNet/OrderBook/ProcessQueueItem.cs
@@ -0,0 +1,27 @@
+﻿using CryptoExchangeNet.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace CryptoExchangeNet.OrderBook
+{
+    internal class ProcessQueueItem
+    {
+        public long StartUpdateId { get; set; }
+        public long EndUpdateId { get; set; }
+        public IEnumerable<ISymbolOrderBookEntry> Bids { get; set; } = Array.Empty<ISymbolOrderBookEntry>();
+        public IEnumerable<ISymbolOrderBookEntry> Asks { get; set; } = Array.Empty<ISymbolOrderBookEntry>();
+    }
+
+    internal class InitialOrderBookItem
+    {
+        public long StartUpdateId { get; set; }
+        public long EndUpdateId { get; set; }
+        public IEnumerable<ISymbolOrderBookEntry> Bids { get; set; } = Array.Empty<ISymbolOrderBookEntry>();
+        public IEnumerable<ISymbolOrderBookEntry> Asks { get; set; } = Array.Empty<ISymbolOrderBookEntry>();
+    }
+
+    internal class ChecksumItem
+    {
+        public int Checksum { get; set; }
+    }
+}
---
+++ CryptoExchangeNet/Sockets/WebsocketFactory.cs
@@ -0,0 +1,17 @@
+﻿using CryptoExchangeNet.Interfaces;
+using CryptoExchangeNet.Logging;
+
+namespace CryptoExchangeNet.Sockets
+{
+    /// <summary>
+    /// Default websocket factory implementation
+    /// </summary>
+    public class WebsocketFactory : IWebsocketFactory
+    {
+        /// <inheritdoc />
+        public IWebsocket CreateWebsocket(Log log, WebSocketParameters parameters)
+        {
+            return new CryptoExchangeWebSocketClient(log, parameters);
+        }
+    }
+}
